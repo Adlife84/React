f3f077cb64a4c73ecb582b1c68d54b5226248131
Andrei <andreykomolov@gmail.com>
2020-05-12 16:14:13 -0400

    14-CSS-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img src='https://www.jdfulwiler.com/wp-content/uploads/2017/08/abc-company-test-logo.png' alt='logo'></img>
-     </header>
+    </header>
 }
 
 export default Header;
