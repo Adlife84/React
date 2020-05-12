813bc0f723935b4e14079e2b2be267cc16529f3c
Andrei <andreykomolov@gmail.com>
2020-05-12 13:15:31 -0400

    12 - lesson

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://www.jdfulwiler.com/wp-content/uploads/2017/08/abc-company-test-logo.png' alt='logo'></img>
+     </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        Main content
+  </div>
+}
+
+
+export default Profile;
