d9cf0cd2ac958460319e317a552a962a62f1d93b
Andrei <andreykomolov@gmail.com>
2020-05-22 13:13:55 -0400

    39-Action creator and action type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import s from'./MyPosts.module.css';
 import Post from './Post/Post';
+import {updateNewPostTextActionCreator} from "../../../redux/state";
+import {addPostActionCreator} from "../../../redux/state";
+
+
 
 const MyPosts = (props) => {
 
@@ -9,14 +13,12 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        // props.addPost();
-        props.dispatch({type: 'ADD-POST'});
+        props.dispatch(addPostActionCreator());
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        // props.updateNewPostText(text);
-        props.dispatch({type: "UPDATE-NEW-POST-TEXT", newText: text})
+        props.dispatch(updateNewPostTextActionCreator(text));
     }
 
     return (
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,109 +1,5 @@
-// let rerenderEntireTree = () => {
-//     console.log('State was change!');
-// }
-//
-// let state = {
-//
-//     profilePage: {
-//         posts: [
-//             {id: 1, message: 'You bro', likes: 10},
-//             {id: 2, message: 'Hi! Hi are you?', likes: 15},
-//             {id: 3, message: 'Awesome! Are you?', likes: 8},
-//             {id: 4, message: 'Is anybody here?', likes: 25}
-//         ],
-//         newPostText: ''
-//     },
-//
-//     dialogsPage: {
-//         dialogs: [
-//             {id: 1, name: 'Andrei'},
-//             {id: 2, name: 'Sveta'},
-//             {id: 3, name: 'Dima'},
-//             {id: 4, name: 'Elena'},
-//             {id: 5, name: 'Valera'},
-//             {id: 6, name: 'Olga'}
-//         ],
-//         messages: [
-//             {id: 1, message: 'Hi how are you?'},
-//             {id: 2, message: 'How is going?'},
-//             {id: 3, message: 'Anydoby here?'},
-//             {id: 4, message: 'Goodbay forever!'}
-//         ],
-//         answers: [
-//             {id: 1, answer: 'I am fine! Are you?'},
-//             {id: 2, answer: 'It is going good'}
-//         ],
-//         newMessageText: 'TestMessage from Stage'
-//     },
-//
-//     sidebar: {
-//         friends: [
-//             {
-//                 id: 1,
-//                 name: 'Vania',
-//                 src: 'https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzkxOTI1MjYy/scarlett-johansson-13671719-1-402.jpg'
-//             },
-//             {
-//                 id: 2,
-//                 name: 'Bread',
-//                 src: 'https://www.kindpng.com/picc/m/28-289524_brad-pitt-png-brad-pitt-transparent-png.png'
-//             },
-//             {id: 3, name: 'Sasha', src: 'https://i.pinimg.com/originals/0a/97/92/0a9792e0e9b396f14b3174d122096a0b.jpg'}
-//         ]
-//     }
-//
-// }
-//
-// window.state = state;
-//
-// export const addPost = () => {
-//
-//     let newPost = {
-//         id: 1,
-//         message: state.profilePage.newPostText,
-//         likes: 0
-//     };
-//
-//     state.profilePage.posts.push(newPost);
-//     state.profilePage.newPostText = '';
-//
-//     rerenderEntireTree(state);
-//
-// }
-//
-// export const updateNewPostText = (newText) => {
-//     state.profilePage.newPostText = newText;
-//     rerenderEntireTree(state);
-// }
-//
-//
-// // function that add messages in Dialogs page /dialog
-// export const addMessage = () => {
-//
-//     let newMessage = {
-//         id: 1,
-//         answer: state.dialogsPage.newMessageText,
-//     };
-//
-//     state.dialogsPage.answers.push(newMessage); //Add new element in array
-//
-//     state.dialogsPage.newMessageText = ''; //Reset value after add in array
-//
-//     rerenderEntireTree(state); //rerender all page
-//
-// }
-//
-// export const updateNewMessageText = (newMessage) => {
-//     state.dialogsPage.newMessageText = newMessage;
-//     rerenderEntireTree(state);
-// }
-//
-// export const subscribe = (observer) => {
-//     rerenderEntireTree = observer;
-// }
-//
-// export default state;
-
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 let store = {
 
@@ -220,8 +116,8 @@ let store = {
 
 
     dispatch(action) { // {type: 'ADD-POST' }
-        
-        if (action.type === "ADD-POST") {
+
+        if (action.type === ADD_POST) {
 
             let newPost = {
                 id: 1,
@@ -234,7 +130,7 @@ let store = {
 
             this._callSubscriber(this._state); //updateNewPostText
 
-        } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
 
@@ -261,7 +157,14 @@ let store = {
 
 }
 
+export const addPostActionCreator = () => ({type: ADD_POST})
+
+export const updateNewPostTextActionCreator = (text) =>
+    ({type: UPDATE_NEW_POST_TEXT, newText: text})
+
+
+
 
 export default store;
 
-window.state = store._state;
+window.store = store;
